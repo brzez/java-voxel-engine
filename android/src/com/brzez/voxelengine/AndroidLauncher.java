29e6d18b075e1b8d3d5a4a0937105d73411c44e1
brzez <pawel@brzez.com>
2016-10-17 07:58:48 +0200

    initial

---
+++ android/src/com/brzez/voxelengine/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brzez.voxelengine;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brzez.voxelengine.VoxelEngine;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VoxelEngine(), config);
+	}
+}
